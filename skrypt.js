afa058c6987d361156519e373a09b0007b5d9e29
Wladyslaw Ko <ever.at.work@gmail.com>
2020-12-21 11:51:03 +0200

    all done but the infinite scrolling effect itself

--- skrypt.js
+++ skrypt.js
@@ -14,3 +14,20 @@ async function getPosts() {
 
     return data;
 }
+
+
+async function showPosts() {
+    const posts = await getPosts();
+    posts.forEach(post => {
+        postEl.classList.add('post');
+        postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+        </div>`
+        postsCont.appendChild(postEl);
+    });
+}
+
+showPosts();
